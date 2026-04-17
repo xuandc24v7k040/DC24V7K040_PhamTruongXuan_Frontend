e35054b26faeaa8287d4d6ee6c783ef94b9569d4
Pham Truong Xuan <xuandc24v7k040@vlvh.ctu.edu.vn>
2026-04-17 19:16:00 +0700

    Feat: Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,16 @@ const routes = [
     name: "contactbook",
     component: ContactBook,
   },
-
   {
     path: "/:pathMatch(.*)*",
     name: "notfound",
     component: () => import("@/views/NotFound.vue"),
+  },
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true,
   },
 ];
 
--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -35,7 +35,7 @@
           <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact="activeContact" />
-        <!-- <router-link
+        <router-link
           :to="{
             name: 'contact.edit',
             params: { id: activeContact._id },
@@ -44,7 +44,7 @@
           <span class="mt-2 badge badge-warning">
             <i class="fas fa-edit"></i> Hiệu chỉnh
           </span>
-        </router-link> -->
+        </router-link>
       </div>
     </div>
   </div>
